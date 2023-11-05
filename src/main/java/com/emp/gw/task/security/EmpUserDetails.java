65dfbeceb2a8ff2e4a8aceb627b0981e0e6b9e45
Evgeni Hristov <evgeni.hristov@bbraun.com>
2023-11-05 23:31:10 +0200

    Enable basic authentication

---
+++ src/main/java/com/emp/gw/task/security/EmpUserDetails.java
@@ -0,0 +1,50 @@
+package com.emp.gw.task.security;
+
+import java.util.Collection;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@RequiredArgsConstructor
+public class EmpUserDetails implements UserDetails {
+
+  private static final long serialVersionUID = 1789789878987L;
+
+  private final User user;
+
+  @Override
+  public String getUsername() {
+    return user.getUsername();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return user.isCredentialsNonExpired();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return user.isEnabled();
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(user::getAuthority);
+  }
+}
---
+++ src/main/java/com/emp/gw/task/security/SecurityConfig.java
@@ -0,0 +1,140 @@
+package com.emp.gw.task.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Supplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
+import org.springframework.util.StringUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  @Bean
+  public UserDetailsService userDetailsService(UserRepository userRepository) {
+    return new EmpUserDetailsService(userRepository);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").authenticated())
+        .authorizeHttpRequests(authorize -> authorize.requestMatchers("/error").permitAll())
+        .csrf(
+            csrf ->
+                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
+        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+        .httpBasic(Customizer.withDefaults());
+    return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    final CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowedOriginPatterns(
+        List.of("http://localhost:[*]", "http://127.0.0.1:[*]"));
+    corsConfiguration.setAllowedHeaders(
+        List.of(
+            "Authorization",
+            "Sec-Fetch-Dest",
+            "Content-Type",
+            "Access-Control-Request-Method",
+            "Sec-Fetch-Mode",
+            "Sec-Fetch-Site",
+            "Referer",
+            "Origin",
+            "Host",
+            "DNT",
+            "Connection",
+            "Access-Control-Request-Method",
+            "Access-Control-Request-Headers",
+            "Accept-Language",
+            "Accept-Encoding",
+            "Access-Control-Allow-Origin",
+            "Accept",
+            "User-Agent",
+            "sentry-trace"));
+    corsConfiguration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+    corsConfiguration.addExposedHeader("*");
+    corsConfiguration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+    return source;
+  }
+
+  final class SpaCsrfTokenRequestHandler extends CsrfTokenRequestAttributeHandler {
+    private final CsrfTokenRequestHandler delegate = new XorCsrfTokenRequestAttributeHandler();
+
+    @Override
+    public void handle(
+        HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> csrfToken) {
+      /*
+       * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of
+       * the CsrfToken when it is rendered in the response body.
+       */
+      this.delegate.handle(request, response, csrfToken);
+    }
+
+    @Override
+    public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
+      /*
+       * If the request contains a request header, use CsrfTokenRequestAttributeHandler
+       * to resolve the CsrfToken. This applies when a single-page application includes
+       * the header value automatically, which was obtained via a cookie containing the
+       * raw CsrfToken.
+       */
+      if (StringUtils.hasText(request.getHeader(csrfToken.getHeaderName()))) {
+        return super.resolveCsrfTokenValue(request, csrfToken);
+      }
+      /*
+       * In all other cases (e.g. if the request contains a request parameter), use
+       * XorCsrfTokenRequestAttributeHandler to resolve the CsrfToken. This applies
+       * when a server-side rendered form includes the _csrf request parameter as a
+       * hidden input.
+       */
+      return this.delegate.resolveCsrfTokenValue(request, csrfToken);
+    }
+  }
+
+  final class CsrfCookieFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+      CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
+      // Render the token value to a cookie by causing the deferred token to be loaded
+      csrfToken.getToken();
+
+      filterChain.doFilter(request, response);
+    }
+  }
+}
